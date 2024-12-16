8fe6e1be918334d59cfc1f9bb245aee8770ba28d
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-12-16 01:46:09 +0100

    try to search and add Norwegian AS to sqlite db

--- src/main.py
+++ src/main.py
@@ -23,7 +23,11 @@ def setup():
       FOREIGN KEY (category_id) REFERENCES category(id)
   );
   """)
-
+  
+  cur.execute("INSERT INTO category (name) VALUES ('private-sector')")
+  cur.execute("INSERT INTO category (name) VALUES ('public-sector')")
+  cur.execute("INSERT INTO category (name) VALUES ('other')")
+    
   con.commit()
   con.close()
 
@@ -31,19 +35,26 @@ def load_json_file(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
     
-def feed_json_to_db():
-   data = load_json_file("merged_github_orgs.json")
-   count = 0
-   for org in data:
-      name = org["name"]
-      if "-as" in name:
-         count += 1
-         print(f"{name} {count}")
-         #pretty_output = json.dumps(org, indent=4)
-         #print(pretty_output) 
-   print(count)
+def find_orgs(search_term, category_id):
+    data = load_json_file("merged_github_orgs.json")
+    count = 0
+    for org in data:
+       name = org["name"]
+       if search_term in name.lower():
+          count += 1
+          #print(f"{name} {count}")
+          pretty_output = json.dumps(org, indent=4)
+          cur.execute("INSERT INTO github_org (name, url, username, user_id, category_id) VALUES (?, ?, ?, ?, ?)", (org["name"], org["html_url"], org["login"], org["id"], category_id))
+          print(pretty_output)
+         
+    con.commit()
+    con.close()
+    print(count)
+   
+def feed_json_to_db(): 
+    find_orgs("-as", 1)
 
 #setup()
 feed_json_to_db()
-
+#TODO: complete this 
 
